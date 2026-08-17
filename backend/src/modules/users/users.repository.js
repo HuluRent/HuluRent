59142483670db00631846bc81afbcac61c257975
FADED <mahletgetinet17@gmail.com>
2026-08-17 22:31:09 +0300

    feat: migrate backend database setup branch

--- backend/src/modules/users/users.repository.js
+++ backend/src/modules/users/users.repository.js
@@ -1,7 +1,2 @@
-const prisma = require('../../config/database');
-
-exports.findUserWithProfile = (userId) =>
-  prisma.user.findUnique({ where: { id: userId }, include: { profile: true } });
-
-exports.updateProfile = (userId, data) =>
-  prisma.profile.update({ where: { userId }, data });
+// Prisma queries for User/Profile
+// TODO: implement
